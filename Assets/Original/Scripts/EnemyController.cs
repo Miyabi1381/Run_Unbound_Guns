790cfc5552da05406bd8969d7b19cbcb8b8c5452
Frira <it252176@trident.ac.jp>
2026-04-03 12:30:25 +0900

    一旦生成器完成！今現状ステージ上に生成されるように制限するのをまだ行ってないからそこ！

--- Assets/Original/Scripts/EnemyController.cs
+++ Assets/Original/Scripts/EnemyController.cs
@@ -1,16 +1,82 @@
-using UnityEngine;
+﻿using UnityEngine;
 
 public class EnemyController : MonoBehaviour
 {
+    [Tooltip("Y軸(高さ)を固定するための変数")]
+    [HideInInspector] private float fixedY;
+
+    [Tooltip("プレイヤーの情報")]
+    Transform player;
+    [Tooltip("歩行速度")]
+    public float speed = 1;
+    [Tooltip("プレイヤーに近づいたときに移動をやめる距離")]
+    public float nearStopDistance = 1;
+    [Tooltip("プレイヤーから離れたときに移動(追従)をやめる距離")]
+    public float farStopDistance = 10;
+    [Tooltip("攻撃する間隔")]
+    public float fightInterval = 1;
+    [Tooltip("攻撃間隔を測るタイマー")]
+    [HideInInspector] public float timer;
+
+    [Tooltip("デバッグ用、攻撃インターバルの確認用カウンター")]
+    [HideInInspector] public int intervalCount;
+
+
+
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        
+        // キャッシュを一度だけ取得
+        player = PlayerMovementScript.instance.transform;
+        // 敵の初期位置を取得
+        fixedY = this.transform.position.y;
+
     }
 
     // Update is called once per frame
     void Update()
     {
-        Vector3 playerPos = PlayerMovementScript.instance.transform.position;
+        // 敵からプレイヤーへの方向を取得(最適化)し、高さを固定
+        Vector3 dir = new Vector3(
+            player.position.x - this.transform.position.x,
+            0f,
+            player.position.z - this.transform.position.z
+            );
+
+        // 方向が0でないなら、プレイヤーの方向に正面を向ける
+        if (dir != Vector3.zero)
+        {
+            transform.forward = dir;
+        }
+
+        // 今いる位置がプレイヤーを追従する範囲内であれば移動処理を行う (memo: .sqrMagnitudeは平方根²の計算)
+        if (dir.sqrMagnitude > nearStopDistance * nearStopDistance
+            && dir.sqrMagnitude < farStopDistance * farStopDistance)
+        {
+            // 敵を移動させる（軽量化のため直接座標を設定）
+            Vector3 pos = this.transform.position;
+            pos += dir.normalized * speed * Time.deltaTime;
+            pos.y = fixedY; // ←ここ超重要
+
+            this.transform.position = pos;
+        }
+
+        // プレイヤーとの距離が一定値以内であれば追従せずとどまる（追加要素：攻撃する）
+        else if (dir.sqrMagnitude < nearStopDistance * nearStopDistance)
+        {
+            // インターバル付き攻撃動作
+            timer += Time.deltaTime;
+            if (timer >= fightInterval)
+            {
+                // タイマーをリセット
+                timer = 0;
+                intervalCount++;
+
+                // 攻撃動作
+                Debug.Log("敵の攻撃を食らった！" + intervalCount);
+            }
+
+        }
+
     }
 }
--- Assets/Original/Scripts/EnemySpawner.cs
+++ Assets/Original/Scripts/EnemySpawner.cs
@@ -1,40 +1,68 @@
-﻿using UnityEditor;
+﻿using System;
+using UnityEditor;
 using UnityEngine;
 using TMPro;
 
 public class EnemySpawner : MonoBehaviour
 {
-    // エネミースポナー
+    // プレイヤー
+    [Tooltip("プレイヤーの情報")]
+    Transform player;
+
+    // 敵
+    [Tooltip("敵プレファブを登録")]
+    public EnemyController enemyPrefab;
+
+    // スポナーのカウント
     [Tooltip("ステージクリア条件である1ステージのスポーン上限")]
     public int spawnUpperLimit;
     [Tooltip("一度にスポーンできる上限")]
     public int spawnLimit;
-    [Tooltip("これまでにスポーンした総数")]
-    [HideInInspector] public int spawnedCount;
+    [Tooltip("これまでにキルした総数")]
+    [HideInInspector] public int killCount;
     [Tooltip("現在スポーンしている数")]
     [HideInInspector] public int currentSpawnCount;
     [Tooltip("スポーンする間隔")]
     public float spawnInterval = 1;
     [Tooltip("スポーン間隔を測るタイマー")]
     [HideInInspector] public float timer;
-    [Tooltip("敵プレファブを登録")]
-    public EnemyController enemyPrefab;
+
+    // スポナーの範囲
+    [Tooltip("スポーンできる最短距離")]
+    public float nearSpawnDistance = 20;
+    [Tooltip("スポーンできる最長距離")]
+    public float farSpawnDistance  = 40;
+    [Tooltip("ステージ上でスポーンすることのできる範囲(最小X)")]
+    public float stageMinX = -50;
+    [Tooltip("ステージ上でスポーンすることのできる範囲(最大X)")]
+    public float stageMaxX =  50;
+    [Tooltip("ステージ上でスポーンすることのできる範囲(最小Z)")]
+    public float stageMinZ = -50;
+    [Tooltip("ステージ上でスポーンすることのできる範囲(最大Z)")]
+    public float stageMaxZ =  50;
 
     // UI
-    [Tooltip("UI（敵の数）のオブジェクト")]
+    [Tooltip("UI（敵の数）のテキストオブジェクト")]
     GameObject enemyTxt;
-    [Tooltip("UI（ウェーブ数）のオブジェクト")]
+    [Tooltip("UI（ウェーブ数）のテキストオブジェクト")]
     GameObject waveTxt;
-
+    [Tooltip("GameClearのテキストオブジェクト")]
+    GameObject clearTxt;
 
     // Start is called once before the first execution of Update after the MonoBehaviour is created
     void Start()
     {
-        // 変数とテキストを結び付けて画面に表示する+テキストを更新
+        // プレイヤーのキャッシュを一度だけ取得
+        player = PlayerMovementScript.instance.transform;
+
+
+        // 変数とテキストを結び付けてクリア以外画面に表示する+テキストを更新
         enemyTxt = GameObject.Find("EnemyCounts");
         waveTxt  = GameObject.Find("WaveCounts");
+        clearTxt = GameObject.Find("GameClear");
         enemyTxt.SetActive(true);
         waveTxt.SetActive(true);
+        clearTxt.SetActive(false);
         UIUpdate();
 
         // 敵をスポーンさせる
@@ -52,8 +80,14 @@ public class EnemySpawner : MonoBehaviour
             // タイマーをリセットする
             timer = 0;
 
+            // スポーン上限に達したならクリア表示をする
+            if (killCount == spawnUpperLimit)
+            {
+                clearTxt.SetActive(true);
+            }
+
             // スポーン上限に満たしていないならスポーンさせる
-            if ((spawnedCount < spawnUpperLimit)
+            if ((killCount < spawnUpperLimit)
                 && (currentSpawnCount < spawnLimit))
             {
                 InstantiateEnemy();
@@ -66,8 +100,11 @@ public class EnemySpawner : MonoBehaviour
     // 敵を生成する
     private void InstantiateEnemy()
     {
+        // スポーンする位置を取得
+        Vector3 spawnPos = GetSpawnPos();
+
         // 敵をスポーンさせる
-        EnemyController enemy = Instantiate(enemyPrefab);
+        EnemyController enemy = Instantiate(enemyPrefab, spawnPos, Quaternion.identity);
 
         // アタッチされたHPスクリプトを取得する
         HPScript hp = enemy.GetComponent<HPScript>();
@@ -78,21 +115,45 @@ public class EnemySpawner : MonoBehaviour
             hp.onDeath += () =>
             {
                 currentSpawnCount--;
+                killCount++;
+                UIUpdate();
             };
 
         }
 
         // スポーンカウントを足す+テキストを更新
         currentSpawnCount++;
-        spawnedCount++;
-        UIUpdate();
+    }
+
+
+    // スポーンさせる位置をランダムに取得
+    public Vector3 GetSpawnPos()
+    {
+        float angle = UnityEngine.Random.Range(0f, 360f);                                   // プレイヤーを中心にスポーンする方向(角度)
+        float distance   = UnityEngine.Random.Range(nearSpawnDistance, farSpawnDistance);   // プレイヤーを中心にスポーンする範囲(距離)
+
+        // 情報を[角度]と[距離]から[座標]に変換する
+        float x = Mathf.Cos(angle * Mathf.Deg2Rad) * distance;
+        float z = Mathf.Sin(angle * Mathf.Deg2Rad) * distance;
+        return new Vector3(
+            player.position.x + x,
+            enemyPrefab.transform.position.y,
+            player.position.z + z
+            );
+    }
+
+
+    // スポーンできる範囲かどうかを判断する(ステージ上かどうか)
+    public bool IsSpawn()
+    {
+        return true;
     }
 
 
     // UIを更新する
     private void UIUpdate()
     {
-        enemyTxt.GetComponent<TextMeshProUGUI>().text = (spawnUpperLimit - spawnedCount + 1) + "/" + spawnUpperLimit;
-        waveTxt.GetComponent<TextMeshProUGUI>().text = "1";
+        enemyTxt.GetComponent<TextMeshProUGUI>().text = (spawnUpperLimit - killCount) + "/" + spawnUpperLimit;
+        waveTxt.GetComponent<TextMeshProUGUI>().text  = "Wave " + "1";
     }
 }

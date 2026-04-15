b8c335e231b320ad17c94d9c2985cb095992f842
Frira <it252176@trident.ac.jp>
2026-04-15 10:07:10 +0900

    BGM/SEを導入。実装のために専用スクリプトを製作中。BGMとSEがエレメント登録できるように配列にしている。

---
+++ Assets/Original/Scripts/AudioSources.cs
@@ -0,0 +1,15 @@
+using UnityEngine;
+
+public class AudioSources : MonoBehaviour
+{
+    // 音のソース
+    AudioSource[] bgmSources;
+    AudioSource[] seSources;
+
+    // Start is called once before the first execution of Update after the MonoBehaviour is created
+    void Start()
+    {
+    }
+
+
+}
